68b0432d02138b7100793faf93c7209f8052ebb3
jmcob <77777490+jmcob@users.noreply.github.com>
2022-07-16 22:00:08 +0200

    blue init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,19 @@
+export default {
+  // These are app level configs.
+  lang: "fr",
+  title: "jmx.Blue",
+  description: "Quelques mots",
+  themeConfig: {
+    nav: [
+      { text: "Crying", link: "/crying" },
+      {
+        text: "Dropdown Menu",
+        items: [
+          { text: "Item A", link: "/item-1" },
+          { text: "Item B", link: "/item-2" },
+          { text: "Item C", link: "/item-3" },
+        ],
+      },
+    ],
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,4 @@
+# welcome to Blue
+Cette page grandira.
+
+
