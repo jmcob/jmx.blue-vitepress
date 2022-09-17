6887538ab220fe12c3e8e40b738daa399bf64c12
Jean-Maxime <ola@jmax.dev>
2022-09-17 10:07:05 +0200

    maj vitepress

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,111 +1,129 @@
 export default {
-  // These are app level configs.
-  lang: "fr",
-  title: "jmx.blue",
-  description: "Quelques mots",
-  themeConfig: {
-    lastUpdatedText: "Dernière mise à jour",
-    logo: "/open-book.png",
-    nav: [
-      {
-        text: "Poèmes",
-        items: [
-          { text: "Bourrasque", link: "/poemes/bourrasque" },
-          { text: "Les lys abandonnés", link: "/poemes/lysAbandonnes" },
-          { text: "Je pleure des oreilles", link: "/poemes/crying" },
-          { text: "Sans titre", link: "/poemes/hypnose" },
+    // These are app level configs.
+    lang: "fr",
+    title: "jmx.blue",
+    description: "Quelques mots",
+    themeConfig: {
+        lastUpdatedText: "Dernière mise à jour",
+        logo: "/open-book.png",
+        nav: [
+            {
+                text: "Poèmes",
+                items: [
+                    { text: "Bourrasque", link: "/poemes/bourrasque" },
+                    {
+                        text: "Les lys abandonnés",
+                        link: "/poemes/lysAbandonnes",
+                    },
+                    { text: "Je pleure des oreilles", link: "/poemes/crying" },
+                    { text: "Sans titre", link: "/poemes/hypnose" },
+                ],
+            },
+            {
+                text: "Textes courts",
+                items: [
+                    { text: "Introduction", link: "/textes/intro" },
+                    { text: "Donner et demander", link: "/textes/giveAndAsk" },
+                    {
+                        text: "Toujours rendre grace",
+                        link: "/textes/rendreGrace",
+                    },
+                    {
+                        text: "Consécration à l'Amour de Dieu",
+                        link: "/textes/consecration",
+                    },
+                    { text: "La comète de Halley", link: "/textes/halley" },
+                ],
+            },
+            {
+                text: "Essais",
+                items: [
+                    {
+                        text: "Il y a une mère qui t'aime",
+                        link: "/essais/marie.md",
+                    },
+                    {
+                        text: "La liberté dans la charité",
+                        link: "/essais/liberteCharite.md",
+                    },
+                ],
+            },
+            {
+                text: "Bible",
+                items: [
+                    {
+                        text: "www.uneparole.love",
+                        link: "http://www.uneparole.love/",
+                    },
+                    { text: "Ephésiens 3,14-19", link: "/bible/ep314" },
+                ],
+            },
+            {
+                text: "Plus",
+                items: [
+                    { text: "Code", link: "https://www.jmax.dev/" },
+                    { text: "Art", link: "https://www.jmxi.art/" },
+                    { text: "A propos", link: "/about" },
+                ],
+            },
         ],
-      },
-      {
-        text: "Textes courts",
-        items: [
-          { text: "Introduction", link: "/textes/intro" },
-          { text: "Donner et demander", link: "/textes/giveAndAsk" },
-          { text: "Toujours rendre grace", link: "/textes/rendreGrace" },
-          {
-            text: "Consécration à l'Amour de Dieu",
-            link: "/textes/consecration",
-          },
-          { text: "La comète de Halley", link: "/textes/halley" },
-        ],
-      },
-      {
-        text: "Essais",
-        items: [
-          { text: "Il y a une mère qui t'aime", link: "/essais/marie.md" },
-          {
-            text: "La liberté dans la charité",
-            link: "/essais/liberteCharite.md",
-          },
-        ],
-      },
-      {
-        text: "Bible",
-        items: [
-          {
-            text: "www.uneparole.love",
-            link: "http://www.uneparole.love/",
-          },
-          { text: "Ephésiens 3,14-19", link: "/bible/ep314" },
-        ],
-      },
-      {
-        text: "Plus",
-        items: [
-          { text: "Code", link: "https://www.jmax.dev/" },
-          { text: "Art", link: "https://www.jmxi.art/" },
-          { text: "A propos", link: "/about" },
-        ],
-      },
-    ],
-    sidebar: [
-      {
-        text: "Textes courts",
-        items: [
-          { text: "Introduction", link: "/textes/intro" },
-          { text: "Donner et demander", link: "/textes/giveAndAsk" },
-          { text: "Toujours rendre grace", link: "/textes/rendreGrace" },
-          {
-            text: "Consécration à l'Amour de Dieu",
-            link: "/textes/consecration",
-          },
-          { text: "La comète de Halley", link: "/textes/halley" },
-        ],
-      },
-      {
-        text: "Poèmes",
-        items: [
-          { text: "Bourrasque", link: "/poemes/bourrasque" },
-          { text: "Les lys abandonnés", link: "/poemes/lysAbandonnes" },
-          { text: "Je pleure des oreilles", link: "/poemes/crying" },
-          { text: "Sans titre", link: "/poemes/hypnose" },
-        ],
-      },
+        sidebar: [
+            {
+                text: "Textes courts",
+                items: [
+                    { text: "Introduction", link: "/textes/intro" },
+                    { text: "Donner et demander", link: "/textes/giveAndAsk" },
+                    {
+                        text: "Toujours rendre grace",
+                        link: "/textes/rendreGrace",
+                    },
+                    {
+                        text: "Consécration à l'Amour de Dieu",
+                        link: "/textes/consecration",
+                    },
+                    { text: "La comète de Halley", link: "/textes/halley" },
+                ],
+            },
+            {
+                text: "Poèmes",
+                items: [
+                    { text: "Bourrasque", link: "/poemes/bourrasque" },
+                    {
+                        text: "Les lys abandonnés",
+                        link: "/poemes/lysAbandonnes",
+                    },
+                    { text: "Je pleure des oreilles", link: "/poemes/crying" },
+                    { text: "Sans titre", link: "/poemes/hypnose" },
+                ],
+            },
 
-      {
-        text: "Essais",
-        items: [
-          { text: "Il y a une mère qui t'aime", link: "/essais/marie.md" },
-          {
-            text: "La liberté dans la charité",
-            link: "/essais/liberteCharite.md",
-          },
+            {
+                text: "Essais",
+                items: [
+                    {
+                        text: "Il y a une mère qui t'aime",
+                        link: "/essais/marie.md",
+                    },
+                    {
+                        text: "La liberté dans la charité",
+                        link: "/essais/liberteCharite.md",
+                    },
+                ],
+            },
+            {
+                text: "Bible",
+                items: [
+                    {
+                        text: "www.uneparole.love",
+                        link: "http://www.uneparole.love/",
+                    },
+                    { text: "Ephésiens 3,14-19", link: "/bible/ep314" },
+                ],
+            },
         ],
-      },
-      {
-        text: "Bible",
-        items: [
-          {
-            text: "www.uneparole.love",
-            link: "http://www.uneparole.love/",
-          },
-          { text: "Ephésiens 3,14-19", link: "/bible/ep314" },
-        ],
-      },
-    ],
-    footer: {
-      message:
-        "Version 1, site en ligne depuis juillet 2022. Textes datant de 2016 à aujourd'hui.",
+        footer: {
+            message:
+                "Version 1, site en ligne depuis juillet 2022. Textes datant de 2016 à aujourd'hui.",
+        },
     },
-  },
 };
---
+++ docs/bible/grosPains 2.md
@@ -0,0 +1,17 @@
+# Gros pains de vie
+
+Vous trouverez ici en PDF de [plus gros pains de vie libres de droit](http://jmxi.art/pains/148-pains-de-vie.pdf), sélectionnés par notre équipe d'adorateurs du très saint sacrement du Sacré-Coeur de Bordeaux.
+
+Ils sont plus gros, plus nourrissant, pour une meilleure prière avec
+notre Seigneur !
+
+Ils sont destinés à être utilisés dans l'Eglise.  
+*Redistribuables à volonté dans les communautés religieuses.*
+
+Il y a 83 pains issus de psaumes et 65 issus du reste de la Bible.
+
+<style scoped>
+em {
+    font-size: large;
+}
+</style>
